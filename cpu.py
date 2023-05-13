b4fd70be6001dbbc4a40f4d65bdfc94a4344f671
Frosty <108729205+LordFrostwolf@users.noreply.github.com>
2023-05-13 16:30:44 +0200

    Implement cpu

---
+++ cpu.py
@@ -0,0 +1,28 @@
+from program_parser import program_parser
+from register import register
+from alu import Alu
+
+class cpu:
+    def __init__(self, program_file: str) -> None:
+        self.command_list = []
+        self.label_lookup = {}
+        self.parser = program_parser(program_file)
+        self.reg = register()
+        self.alu = Alu(self.reg, self)
+        self.con = True
+
+    def run(self):
+        command_tuple = self.parser.parse()
+        self.command_list = command_tuple[0]
+        self.label_lookup = command_tuple[1]
+
+        self.reg.write("pc",self.label_lookup["main"])
+
+        while self.con:
+            self.alu.command(self.command_list[self.reg.read("pc")])
+
+    def branch(self, label:str):
+        pass
+
+    def end(self):
+        self.con = False
